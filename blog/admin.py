16cedc0b5625603dd9502f7cd112732312a7439b
fionatreacy <fionatreacy00@gmail.com>
2022-11-07 16:19:17 +0000

    add post detail and styling

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,19 @@
+@@ -1,14 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
-
-# Register your models here.
-
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('created_on',)
     list_display = ('title', 'slug', 'created_on')
+    list_display = ('title', 'slug', 'location','created_on')
     search_fields = ['title', 'content']
     summernote_fields = ('content')
 
-
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'body', 'post', 'created_on')
     list_filter = ('created_on',)
     search_fields = ['name', 'body']
