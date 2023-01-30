1287c5f40f3b11c60fe207609b74ea26e9115a01
fionatreacy <fionatreacy00@gmail.com>
2023-01-30 17:38:13 +0000

    add coments

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
-@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -11,11 +9,8 @@ class PostList(generic.ListView):
     queryset = Post.objects.order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
-
-
-
+   
 class PostDetail(View):
-
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects
         post = get_object_or_404(queryset, slug=slug)
@@ -23,7 +18,7 @@ class PostDetail(View):
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-
+        
         return render(
             request,
             "post_detail.html",
@@ -32,4 +27,36 @@ class PostDetail(View):
                 "comments": comments,
                 "liked": liked
             }
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
         )
