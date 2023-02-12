a3f7fd3c0bf9c899076e0b151140b31eddddb649
fionatreacy <fionatreacy00@gmail.com>
2023-02-12 12:35:21 +0000

    update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,19 +1,27 @@
-@@ -1,14 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    """
+    Add fields which will use summernote editor in admin page
+    """
 
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('created_on',)
-    list_display = ('title', 'slug', 'created_on')
-    list_display = ('title', 'slug', 'location','created_on')
+    list_display = ('title', 'slug', 'location', 'created_on')
     search_fields = ['title', 'content']
     summernote_fields = ('content')
 
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Add fields for display in admin page
+    """
+
     list_display = ('name', 'body', 'post', 'created_on')
     list_filter = ('created_on',)
     search_fields = ['name', 'body']
